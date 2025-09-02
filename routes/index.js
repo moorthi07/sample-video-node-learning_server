7898f3cf2dc513b000a4404b1608a09fb34e4698
Dwane Hemmings <dwane.hemmings@vonage.com>
2025-09-02 13:15:56 -0400

    Update audioRate to 16000 in websocket config

--- routes/index.js
+++ routes/index.js
@@ -500,7 +500,7 @@ router.post('/audio-connector/connect', async (req, res) => {
         "token": webSocketToken,
         "websocket": {
           "uri": `wss://${webSocketHost}`,
-          "audioRate": 8000,
+          "audioRate": 16000,
           "bidirectional": true
         }
       })
